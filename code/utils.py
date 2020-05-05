c8c8bfbe20cfbf174803433e9f94946fcbcc3ac5
Marvin Buss <marvin.buss@gmail.com>
2020-05-05 23:48:50 +0200

    bugfix

--- code/utils.py
+++ code/utils.py
@@ -11,8 +11,8 @@ class AMLDeploymentException(Exception):
 
 def validate_json(data, schema, input_name):
     validator = jsonschema.Draft7Validator(schema)
-    errors = validator.iter_errors(data)
-    if len(list(errors)) > 0:
+    errors = list(validator.iter_errors(data))
+    if len(errors) > 0:
         for error in errors:
             print(f"::error::JSON validation error: {error}")
         raise AMLConfigurationException(f"JSON validation error for '{input_name}'. Provided object does not match schema. Please check the output for more details.")
